9528583a20f4c76e031f78e8c9d0105518b70d44
Alihan Abiteev <abiteeev.ali@gmail.com>
2024-04-06 21:12:35 +0600

    Initial Commit

---
+++ LeaderID/LeaderIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaderIDApp.swift
+//  LeaderID
+//
+//  Created by Alihan Abiteev on 06.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LeaderIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
